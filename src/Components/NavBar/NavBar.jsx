18be8770ed020b693bf155c898f852440c16482b
Gaston Scocco <gaston_scocco@hotmail.com>
2021-12-08 19:48:00 -0300

    First Commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ function NavBar() {
     return (
         <div className='flex bg-green-200 h-12 items-center justify-center fixed w-screen' >
             This is the Nav
+            <button>Take All</button>
         </div>
     )
 }
