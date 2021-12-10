73919e5e4c15ccd319d7e060eee35e8bb5b0252b
Gaston Scocco <gaston_scocco@hotmail.com>
2021-12-10 01:10:33 -0300

    main

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className='flex items-center justify-around h-12 bg-red-200'>
-            This is the footer
+            This is the footer s
         </div>
     )
 }
